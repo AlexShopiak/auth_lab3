6f962dbdaa4965e2a9c2e688f0fdd408676a9028
Alex Shopiak <alshop2004@gmail.com>
2024-05-19 14:16:23 +0000

    Implement getUserTokens

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 const axios = require('axios');
 
+async function getUserTokens(domain, audience, client_id, client_secret, username, password) {
+  const data = {
+    grant_type: 'password',
+    audience: audience,
+    client_id: client_id,
+    client_secret: client_secret,
+    username: username,
+    password: password,
+    scope: 'offline_access'
+  };
+
+  const url = 'https://' + domain +'/oauth/token';
+
+  try {
+    const response = await axios.post(url, data, {
+      headers: {'content-type': 'application/x-www-form-urlencoded'}
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error getting user token', error.response ? error.response.data : error.message);
+    //throw error;
+  }
+}
+
