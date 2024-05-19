833a3109d646039a73127ca818a3293c4244e6a7
Alex Shopiak <alshop2004@gmail.com>
2024-05-19 17:57:53 +0000

    CopyPaste getToken function with client_creditential grant

--- index2.js
+++ index2.js
@@ -1,2 +1,22 @@
 const axios = require("axios");
 
+async function getToken(domain, client_id, client_secret, audience) {
+    const options = {
+    method: 'POST',
+    url: `https://${domain}/oauth/token`,
+    headers: {'content-type': 'application/x-www-form-urlencoded'},
+    data: new URLSearchParams({
+        grant_type: 'client_credentials',
+        client_id: client_id,
+        client_secret: client_secret,
+        audience: audience
+    })
+    };
+    try {
+        const response = await axios(options);
+        return response.data;
+    } catch (error) {
+        console.error('Error changing password:', error.response ? error.response.data : error.message);
+    }
+}
+
